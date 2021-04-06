53793717e1725bd627a3134bdbd4707fbfa1a29b
Victor Valdez <victor334_@hotmail.com>
2021-04-05 23:06:48 -0400

    inital commit: project initialized

---
+++ XamarinUtilsSample/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Prism;
+using Prism.Ioc;
+using Prism.Unity;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinUtilsSample.ViewModels;
+using XamarinUtilsSample.Views;
+
+namespace XamarinUtilsSample
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer platformInitializer = null) : base(platformInitializer) { }
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+            await NavigationService.NavigateAsync("NavigationPage/HomePage");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<HomePage>();
+            containerRegistry.RegisterForNavigation<DeviceOrientationPage, DeviceOrientationViewModel>();
+     
+            
+
+        }
+
+    }
+}
